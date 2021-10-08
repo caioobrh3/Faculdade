4c57b6a50b452ca13312d889adbdfcc485271784
Caio Ricardo <contatocaioobr@hotmail.com>
2021-10-08 08:10:30 -0300

    Update Pratica20.java

--- aula/src/br/ucsal/Pratica20.java
+++ aula/src/br/ucsal/Pratica20.java
@@ -3,8 +3,13 @@ package br.ucsal;
 public class Pratica20 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int nmr=1;
+		System.out.println(nmr);
+		while(nmr<=24) {
+			if(nmr%2==0) {
+				System.out.println(nmr);
+			}
+			nmr++;
+		}
 	}
-
 }
