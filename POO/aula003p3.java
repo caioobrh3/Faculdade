66466700733284f39edb94057637e0a5396203bc
Caio Ricardo <83782001+caioobrh3@users.noreply.github.com>
2023-03-07 08:42:04 -0300

    Add files via upload

---
+++ POO/aula003p3.java
@@ -0,0 +1,28 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class aula003p3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Qual a idade do nadador? ");
+        int idade = input.nextInt();
+
+        exibirCategoriaNadador(idade);
+    }
+
+    public static void exibirCategoriaNadador(int idade) {
+        if (idade >= 5 && idade <= 7) {
+            System.out.println("Infantil A");
+        } else if (idade >= 8 && idade <= 10) {
+            System.out.println("Infantil B");
+        } else if (idade >= 11 && idade <= 13) {
+            System.out.println("Juvenil A");
+        } else if (idade >= 14 && idade <= 17) {
+            System.out.println("Juvenil B");
+        } else {
+            System.out.println("Sênior");
+        }
+    }
+}
