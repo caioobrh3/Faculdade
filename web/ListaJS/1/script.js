b6e856b69d02049dfc0b93458edc0de7440095e0
caioricardoGTI <contatocaioobr@hotmail.com>
2023-09-06 08:50:10 -0300

    Ajuste

--- web/ListaJS/1/script.js
+++ web/ListaJS/1/script.js
@@ -1,25 +1,20 @@
-// Variáveis
 var a = 10;
 var b = 5;
 var c = 1;
 var d;
 
-// a) Operações
 c = b--;
 b = a++;
 a = 5 + "1";
 d = "1" + "1";
 
-// Atualizar o conteúdo HTML com os valores das variáveis
 document.getElementById("valorA").textContent = a;
 document.getElementById("valorB").textContent = b;
 document.getElementById("valorC").textContent = c;
 document.getElementById("valorD").textContent = d;
 
-// b) Informações sobre o autor
 var autor = "Caio Ricardo Lincoln Afonso";
 var palavraChave = "JavaScript";
 var descricao = "Atividades sobre JavaScript";
 
-// c) Título da página
 var tituloPagina = "JavaScript";
