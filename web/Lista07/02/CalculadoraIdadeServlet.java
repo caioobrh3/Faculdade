49c5e1ff6f451858f6b9daaa8d8aece34b4dd0d3
caioricardop <83782001+caioricardop@users.noreply.github.com>
2023-09-27 08:35:49 -0300

    a

--- web/Lista07/02/CalculadoraIdadeServlet.java
+++ web/Lista07/02/CalculadoraIdadeServlet.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Calendar;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,38 +8,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/CalculadoraServlet")
-public class CalculadoraServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+@WebServlet("/CalcularIdadeServlet")
+public class CalcularIdadeServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
-        // Obter os valores dos parâmetros do formulário
-        int numero1 = Integer.parseInt(request.getParameter("numero1"));
-        int numero2 = Integer.parseInt(request.getParameter("numero2"));
+        int anoNascimento = Integer.parseInt(request.getParameter("anoNascimento"));
+        String fezAniversario = request.getParameter("fezAniversario");
 
-        // Realizar operações matemáticas
-        int soma = numero1 + numero2;
-        int subtracao = numero1 - numero2;
-        int multiplicacao = numero1 * numero2;
-        int divisao = numero1 / numero2;
-        int resto = numero1 % numero2;
+        Calendar calendario = Calendar.getInstance();
+        int anoAtual = calendario.get(Calendar.YEAR);
 
-        // Exibir os resultados
+        int idade;
+        if ("sim".equals(fezAniversario)) {
+            idade = anoAtual - anoNascimento;
+        } else {
+            idade = anoAtual - anoNascimento - 1;
+        }
+
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<head>");
-        out.println("<title>Resultado</title>");
+        out.println("<title>Idade Atual</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>Resultados</h1>");
-        out.println("<p>Número 1: " + numero1 + "</p>");
-        out.println("<p>Número 2: " + numero2 + "</p>");
-        out.println("<p>Soma: " + soma + "</p>");
-        out.println("<p>Subtração: " + subtracao + "</p>");
-        out.println("<p>Multiplicação: " + multiplicacao + "</p>");
-        out.println("<p>Divisão: " + divisao + "</p>");
-        out.println("<p>Resto da Divisão: " + resto + "</p>");
+        out.println("<h1>Sua Idade Atual</h1>");
+        out.println("<p>Ano de Nascimento: " + anoNascimento + "</p>");
+        out.println("<p>Já fez aniversário este ano? " + fezAniversario + "</p>");
+        out.println("<p>Idade Atual: " + idade + " anos</p>");
         out.println("</body>");
         out.println("</html>");
     }
